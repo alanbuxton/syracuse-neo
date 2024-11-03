16968805348437a6485658cd9f8b616d21acad7d
Alan Buxton <alanbuxton@gmail.com>
2024-11-03 23:16:56 +0000

    Added index to speed up deletions during import
    From 2 minutes to 2 seconds
    
    Nov  3 22:38:59 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:38:59,819 - integration.management.commands.import_ttl - INFO - Before deleting 3428235 nodes. After delete 3427999 nodes
    Nov  3 22:40:57 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:40:57,855 - integration.management.commands.import_ttl - INFO - Before deleting 3427999 nodes. After delete 3427739 nodes
    Nov  3 22:42:46 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:42:46,466 - integration.management.commands.import_ttl - INFO - Before deleting 3427739 nodes. After delete 3427493 nodes
    Nov  3 22:44:57 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:44:57,964 - integration.management.commands.import_ttl - INFO - Before deleting 3427493 nodes. After delete 3427250 nodes
    Nov  3 22:45:09 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:09,357 - integration.management.commands.import_ttl - INFO - Before deleting 3427250 nodes. After delete 3427010 nodes
    Nov  3 22:45:11 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:11,404 - integration.management.commands.import_ttl - INFO - Before deleting 3427010 nodes. After delete 3426766 nodes
    Nov  3 22:45:12 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:12,888 - integration.management.commands.import_ttl - INFO - Before deleting 3426766 nodes. After delete 3426550 nodes
    Nov  3 22:45:14 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:14,555 - integration.management.commands.import_ttl - INFO - Before deleting 3426550 nodes. After delete 3426298 nodes
    Nov  3 22:45:16 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:16,816 - integration.management.commands.import_ttl - INFO - Before deleting 3426298 nodes. After delete 3426066 nodes
    Nov  3 22:45:18 syracuse-ams-01 syracuse-prod-import-ttl: 2024-11-03 22:45:18,305 - integration.management.commands.import_ttl - INFO - Before deleting 3426066 nodes. After delete 3425813 nodes

--- integration/neo4j_utils.py
+++ integration/neo4j_utils.py
@@ -20,6 +20,7 @@ def delete_and_clean_up_nodes_from_doc_id_file(doc_id_file):
 def setup_db_if_necessary():
     db.cypher_query("CREATE CONSTRAINT n10s_unique_uri IF NOT EXISTS FOR (r:Resource) REQUIRE r.uri IS UNIQUE;")
     db.cypher_query("CREATE INDEX node_internal_doc_id_index IF NOT EXISTS FOR (n:Resource) on (n.internalDocId)")
+    db.cypher_query("CREATE INDEX node_merged_same_as_high_to_uri IF NOT EXISTS FOR (n:Resource) on (n.internalMergedSameAsHighToUri)")
     v, _ = db.cypher_query("call n10s.graphconfig.show")
     if len(v) == 0:
         do_n10s_config()
@@ -31,7 +32,7 @@ def do_n10s_config(overwrite=False):
                 "description","foundName","industry",
                 "locationFoundName",
                 "locationPurpose","locationType","name",
-                "nameClean", "orgName", 
+                "nameClean", "orgName",
                 "orgFoundName",
                 "roleFoundName","roleHolderFoundName",
                 "status","targetDetails","targetName","valueRaw",
